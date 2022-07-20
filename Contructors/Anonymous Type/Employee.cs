633f784e27ac9413ca0ecadffcf901211cccad1f
NguyenTienDat23 <103103011+NguyenTienDat23@users.noreply.github.com>
2022-07-20 15:52:43 +0700

    Hoàn thành

---
+++ Contructors/Anonymous Type/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Type
+{
+    internal class Employee
+    {
+    }
+}
